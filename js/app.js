7be24b5e7f601fc00ba7cc8c01f9e5c1717b687d
James-Hardy <jamesjhardy95@gmail.com>
2020-02-09 18:48:25 +0000

    Added HTML, JS & CSS files
    Started off the project by adding in the buttons for the calculator.
    The basic functions are written in the app.js file, they now get the button inputs
    app.css has some styling for the buttons

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function getNumber(num){
+    var input_var = document.getElementById('input');
+    /* Switch(num): 
+    case 1:
+        input_var += '1';
+    case 2: 
+        input_var += '2';  
+    */
+}
+
+function getOperation(operation){
+    
+    /* Switch(operation): 
+     
+    */
+}
+
+function compute(){}
