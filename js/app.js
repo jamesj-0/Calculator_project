bb677b3253fc80cd0987f12c6e40208a973cbc32
James-Hardy <jamesjhardy95@gmail.com>
2020-02-25 00:45:07 +0000

    Changed JS Functions and CSS
    Problems with the compute function, will need to add numbers to an array in order to work properly. Will write out the psuedo code for adding the numbers to array and the functions to compute the sums properly.

--- js/app.js
+++ js/app.js
@@ -1,50 +1,53 @@
-function getNumber(num){
-    var input_var = document.getElementById('input');
-    switch(num){
+function getNumber(num) {
+    var input_var = document.getElementById('display');
+    switch (num) {
         case 1:
-        input_var.value += '1';
-        //console.log(1);
-        break;
-        case 2: 
-        input_var.value += '2'; 
-        break; 
-        case 3: 
-        input_var.value += '3';
-        break;
-        case 4: 
-        input_var.value += '4';
-        break;
-        case 5: 
-        input_var.value += '5';
-        break;
-        case 6: 
-        input_var.value += '6';
-        break;
-        case 7: 
-        input_var.value += '7';
-        break;
-        case 8: 
-        input_var.value += '8';
-        break;
-        case 9: 
-        input_var.value += '9';
-        break;
-        case 0: 
-        input_var.value += '0'; 
-        break;
+            input_var.value += '1';
+            break;
+        case 2:
+            input_var.value += '2';
+            break;
+        case 3:
+            input_var.value += '3';
+            break;
+        case 4:
+            input_var.value += '4';
+            break;
+        case 5:
+            input_var.value += '5';
+            break;
+        case 6:
+            input_var.value += '6';
+            break;
+        case 7:
+            input_var.value += '7';
+            break;
+        case 8:
+            input_var.value += '8';
+            break;
+        case 9:
+            input_var.value += '9';
+            break;
+        case 0:
+            input_var.value += '0';
+            break;
+        case '.':
+            input_var.value += '.';
+            break;
     }
 }
 
-function clearScreen(){
-//when function called input and answer .value = ""
-    document.getElementById('input').value = "";
+function clearScreen() {
+    //when function called input and answer .value = ""
+    document.getElementById('display').value = "";
     document.getElementById('answer').value = "";
+
 }
 
 //get the mathematical sign
-function getOperation(operation){
-    var input_var = document.getElementById('input');
-    switch(operation){
+function getOperation(operation) {
+    var input_var = document.getElementById('display');
+    switch (operation) {
         case '+':
             input_var.value += '+';
             break;
@@ -62,20 +65,23 @@ function getOperation(operation){
     }
 }
 
-// take the input var value and remove the last character
-function backspace(){
-    var input_var = document.getElementById('input');
-    var x = input_var.value; //save the value as new variable
-    if(x.lenght > 0){
-        x = x.substring(0, x.lenght-1); //create a substring from 0 to the length -1
-        input_var.value= x; 
-    }
-
+//using the eval function take the string and compute
+function compute() {
+    var input_var = document.getElementById('display');
+    ans = eval(input_var.value).toFixed(7);
+    document.getElementById('display').value = ans /*.replace(/0+$/,''); //using regex as there were answers with trailing zeros */
 }
 
-//using the eval function take the string and compute
-function compute(){
-    var input_var = document.getElementById('input');
-    ans = eval(input_var.value);
-    document.getElementById('answer').value = '=' + ans;
+var x = 0;
+function brackets() {
+    //when bracket pushed once insert first bracket
+    //when bracket pushed again insert second bracket
+    var input_var = document.getElementById('display');
+    if (x ==0){
+        input_var.value += '(';
+        x = 1;
+    }else if (x == 1) {
+        input_var.value += ')';
+        x = 0;
+    } 
 }
