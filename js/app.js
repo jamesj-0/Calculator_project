4dd7c2786a9e4979ea30b93754b5c029a489612d
James-Hardy <jamesjhardy95@gmail.com>
2020-02-25 17:30:58 +0000

    Fixed compute function
    The compute function now works when you pass the array of values and operation.

--- js/app.js
+++ js/app.js
@@ -1,59 +1,72 @@
 //array to save the number pressed.
-let numDisplay = [];
+let numDisplay = []
 
 //array with 3 elements [1, +, 2]
-let computeArr = []; 
+let computeArr = []
 
 //boolean to see if the last thing pressed was an operation or number
-let lastPressedOp = false;
+let lastPressedOp = false
 
-var numberVal = 0;
+var numberVal = 0
 
 function getNumber(num) {
-    numDisplay.push(num);
-    //display the joined elements of the array.
-    numberVal = numDisplay.join("");
-    document.getElementById('display').value = numberVal;
-    // console.log(numberVal);
-    lastPressedOp = false; // the last pressed button was a number.
+  numDisplay.push(num)
+  //display the joined elements of the array.
+  numberVal = numDisplay.join('')
+  document.getElementById('display').value = numberVal
+  // console.log(numberVal);
+  lastPressedOp = false // the last pressed button was a number.
 }
 
 function clearScreen() {
-    //when function called input and answer .value = ""
-    document.getElementById('display').value = "";
-    document.getElementById('answer').value = "";
-
+  //when function called input and answer .value = ""
+  document.getElementById('display').value = ''
+  document.getElementById('answer').value = ''
 }
 
 //get the mathematical sign
 function getOperation(operation) {
-    if(!lastPressedOp){ // if the last thing pressed is not a number then...
-        lastPressedOp = true;
-        computeArr.push(numberVal); //push number val
-        computeArr.push(operation); //push operation
-        document.getElementById('display').value = operation;
-        console.log(computeArr);
-        numDisplay = []; //reset the display num to empty;
-    }
+  if (!lastPressedOp) {
+    // if the last thing pressed is not a number then...
+    lastPressedOp = true
+    computeArr.push(parseFloat(numberVal, 10)) //push number val
+    computeArr.push(operation) //push operation
+    document.getElementById('display').value = operation
+    console.log(computeArr)
+    numDisplay = [] //reset the display num to empty;
+  }
 }
 
-//using the eval function take the string and compute
+//get rid of the eval function take the string and compute
 function compute() {
-    var input_var = document.getElementById('display');
-    ans = eval(input_var.value).toFixed(7);
-    document.getElementById('display').value = ans /*.replace(/0+$/,''); //using regex as there were answers with trailing zeros */
+  //push the last number
+  computeArr.push(parseFloat(numberVal, 10))
+  // console.log(computeArr);
+  var sum = computeArr[0] // the first value in the array
+  for (var i = 1; i < computeArr.length; i++) {
+    if (computeArr[i - 1] == '+') {
+      sum = sum + computeArr[i]
+    } else if (computeArr[i - 1] == '-') {
+      sum = sum - computeArr[i]
+    } else if (computeArr[i - 1] == '/') {
+      sum = sum / computeArr[i]
+    } else if (computeArr[i - 1] == '*') {
+      sum = sum * computeArr[i]
+    }
+  }
+  console.log(sum)
 }
 
-var x = 0;
+var x = 0
 function brackets() {
-    //when bracket pushed once insert first bracket
-    //when bracket pushed again insert second bracket
-    var input_var = document.getElementById('display');
-    if (x ==0){
-        input_var.value += '(';
-        x = 1;
-    }else if (x == 1) {
-        input_var.value += ')';
-        x = 0;
-    } 
+  //when bracket pushed once insert first bracket
+  //when bracket pushed again insert second bracket
+  var input_var = document.getElementById('display')
+  if (x == 0) {
+    input_var.value += '('
+    x = 1
+  } else if (x == 1) {
+    input_var.value += ')'
+    x = 0
+  }
 }
