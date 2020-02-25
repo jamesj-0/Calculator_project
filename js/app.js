cda9b6222b1d2b9d3c34896f97ac8d49e5ff2dc6
James-Hardy <jamesjhardy95@gmail.com>
2020-02-25 15:48:22 +0000

    Function for get number changed
    The function now saves the numerical value in a variable to be pushed to an array. This array will then be used to calculate the value properly instead of using the eval function

--- js/app.js
+++ js/app.js
@@ -1,40 +1,18 @@
+//array to save the number pressed.
+let numDisplay = [];
+
+//boolean to see if the last thing pressed was an operation or number
+let lastPressedOp = false;
+
+var numberVal = 0;
+
 function getNumber(num) {
-    var input_var = document.getElementById('display');
-    switch (num) {
-        case 1:
-            input_var.value += '1';
-            break;
-        case 2:
-            input_var.value += '2';
-            break;
-        case 3:
-            input_var.value += '3';
-            break;
-        case 4:
-            input_var.value += '4';
-            break;
-        case 5:
-            input_var.value += '5';
-            break;
-        case 6:
-            input_var.value += '6';
-            break;
-        case 7:
-            input_var.value += '7';
-            break;
-        case 8:
-            input_var.value += '8';
-            break;
-        case 9:
-            input_var.value += '9';
-            break;
-        case 0:
-            input_var.value += '0';
-            break;
-        case '.':
-            input_var.value += '.';
-            break;
-    }
+    numDisplay.push(num);
+    //display the joined elements of the array.
+    numberVal = numDisplay.join("");
+    document.getElementById('display').value = numberVal;
+    // console.log(numberVal);
+    lastPressedOp = false; // the last pressed button was a number.
 }
 
 function clearScreen() {
